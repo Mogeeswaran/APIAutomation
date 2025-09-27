c7b26048f5acf0294e169c86f208538f6f4d7d7e
Mogee <mogee1993@gmail.com>
2025-09-27 15:27:47 +0530

    added test validations

--- src/test/java/com/personal/test/Demo1GetPetDetailsTest.java
+++ src/test/java/com/personal/test/Demo1GetPetDetailsTest.java
@@ -1,5 +1,8 @@
 package com.personal.test;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.restassured.RestAssured;
 import org.apache.http.HttpStatus;
 import org.testng.Assert;
@@ -10,7 +13,7 @@ public class Demo1GetPetDetailsTest {
     public String baseUrl = "https://petstore.swagger.io/v2/";
 
     @Test
-    public void findValidPetIdTest() {
+    public void findValidPetIdTest() throws JsonProcessingException {
         int petId = 5;
         String resource = "pet/" + petId;
 
@@ -22,5 +25,14 @@ public class Demo1GetPetDetailsTest {
         System.out.println(response);
 //        Assert.assertTrue(response.contains(":5"),"Not matching id");
 //        response.
+
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode json = mapper.readTree(response);
+
+        System.out.println(json.get("id"));
+        System.out.println(json.get("name").asText());
+        System.out.println(json.get("category").get("id"));
+        System.out.println(json.get("tags").get(0).get("id").asInt());
+
     }
 }
