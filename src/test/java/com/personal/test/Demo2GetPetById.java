5e6be7be4168f6127a315641e517de6b2e844cbc
Mogee <mogee1993@gmail.com>
2025-10-04 13:03:05 +0530

    added model and demo test

---
+++ src/test/java/com/personal/test/Demo2GetPetById.java
@@ -0,0 +1,60 @@
+package com.personal.test;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import org.apache.http.HttpStatus;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import javax.xml.transform.Source;
+
+public class Demo2GetPetById {
+
+    public String baseUrl = "https://petstore.swagger.io/v2/";
+
+    /**
+     * Tree like structure - flexible structure - avoid if planning for schema validation
+     */
+    @Test
+    public void demo1GetPetByIdJsonNode(){
+        int petId=5;
+        String resource="pet/"+petId;
+
+        JsonNode responseJson = RestAssured
+                .given()
+                .when().get(baseUrl + resource)
+                .then().statusCode(HttpStatus.SC_OK).extract().as(JsonNode.class);
+
+        System.out.println(responseJson.get("id"));
+        System.out.println(responseJson.get("name").asText());
+        System.out.println(responseJson.get("category").get("id"));
+        System.out.println(responseJson.get("tags").get(0).get("id").asInt());
+
+        System.out.println(responseJson.get("photoUrls").get(0));
+
+        Assert.assertEquals(responseJson.get("id").asInt(),5);
+    }
+
+    /**
+     * only advatage is json expression. (Not type safety, auto mapping nested objects, not easy to maintain, not suitable for api automation)
+     */
+    @Test
+    public void demo1GetPetByIdJsonPath(){
+        int petId=5;
+        String resource="pet/"+petId;
+
+        JsonPath jsonPathObj = RestAssured
+                .given()
+                .when().get(baseUrl+resource)
+                .then().statusCode(HttpStatus.SC_OK).extract().jsonPath();
+
+        System.out.println(jsonPathObj.prettify());
+        System.out.println(jsonPathObj.getInt("id"));
+        System.out.println(jsonPathObj.getString("name"));
+        System.out.println(jsonPathObj.getString("category.id"));
+        System.out.println(jsonPathObj.getString("tags[0].id"));
+//        System.out.println(jsonPathObj.getList("tags[*].id"));
+
+    }
+}
