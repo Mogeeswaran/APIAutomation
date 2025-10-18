87942bbb84b91ce6c6d475c0f56c0440360f12ab
Mogee <mogee1993@gmail.com>
2025-10-18 15:20:16 +0530

    added yaml and updated method for GIT API

--- src/test/java/com/personal/test/Demo4GitAPITest.java
+++ src/test/java/com/personal/test/Demo4GitAPITest.java
@@ -1,5 +1,6 @@
 package com.personal.test;
 
+import com.atlassian.oai.validator.restassured.OpenApiValidationFilter;
 import io.restassured.path.json.JsonPath;
 import org.testng.annotations.Test;
 import io.restassured.RestAssured;
@@ -14,9 +15,14 @@ public class Demo4GitAPITest {
         FileInputStream fileInputStream = new FileInputStream("src/test/resources/secret.json");
         JsonPath jsonPath = new JsonPath(fileInputStream);
         String token = jsonPath.get("token");
+
+        OpenApiValidationFilter filter = new OpenApiValidationFilter("src/test/resources/gitrepo.yaml");
+
         RestAssured.baseURI="https://api.github.com";
+
         RestAssured
                 .given()
+//                .filter(filter)
                 .headers("Authorization","Bearer "+token)
                 .when().get("/user/repos")
                 .then().statusCode(200).log().all();
